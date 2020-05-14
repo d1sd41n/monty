31b4e04b0fa5a59a40c2a565d793c4dbebf222f6
Daniel Alejandro <outnumbered@live.com>
2020-05-14 00:16:25 -0500

    added execute function

---
+++ file.m
@@ -0,0 +1,4 @@
+push 2 push 0 push add fddfdf
+push 3 ff dfd
+push 3
+pint
--- main.c
+++ main.c
@@ -1,36 +1,69 @@
 #include "monty.h"
- 
-/** monty - checks arguments and reads monty file
-* @argc: number or arguments
-* @argv: array with arguments
-* 
-* Return: 0 on success
-*/
- 
+
+
+
+int execute(char *line[], int isPush, int row, stack_t **top)
+{
+	printf("%s %i  line: %i\n", line[0], isPush, row);
+
+	if (strcmp(line[0], "push") == 0)
+	{
+		add_dnodeint(top, 1);
+	}
+	else if (strcmp(line[0], "pint") == 0)
+	{
+		print_dlistint(*top);
+	}
+
+	return (0);
+
+}
+
+/**
+ * main - main function of shell.
+ * @argc: counter arguments.
+ * @argv: arguments pointer.
+ * Return: final .status.
+ */
 int main(int argc, char *argv[])
 {
 	FILE *fd;
-	/**char *command[256];*/
-	int line_number = 0;
+	int i, isPush, row = 0;
 	size_t buffer_size = 80;
-	char *buffer = malloc(buffer_size * sizeof(char));
-	char *command, *parameter;
+	char *command, *parameter, *line[1024], *buffer;
+	stack_t *top = NULL;
 
 	check_mfile(argc);
 	fd = fopen(argv[1], "r");
+	buffer = malloc(buffer_size * sizeof(char));
+	if (buffer == NULL)
+	{
+		free(buffer);
+		exit(EXIT_FAILURE);
+	}
 	if (fd == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-    	while(-1 != getline(&buffer, &buffer_size, fd))
-    	{
-		command = strtok(buffer, " ");
-		//if (command == 'add')
-		parameter = strtok(buffer, " ");
-		printf("%s %s\n", command, parameter);
+	while (-1 != getline(&buffer, &buffer_size, fd))
+	{
+
+		isPush = 1;
+		command = strtok(buffer, " \n");
+		for (i = 0; i < 3 && command != NULL; i++)
+		{
+			line[i] = command;
+			command = strtok(NULL, " \n");
+			if (strcmp(line[0], "push") == 0)
+				continue;
+			isPush = 0;
+			break;
+		}
+		execute(line, isPush, ++row, &top);
 	}
-    	fflush(stdout);
+	free(buffer);
+	fflush(stdout);
 	fclose(fd);
 	return (0);
 }
