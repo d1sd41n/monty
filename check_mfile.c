1d168057fbc6ee05428c53e8e148286b430d9be3
s-macias <1552@holbertonschool.com>
2020-05-14 01:14:58 +0000

    adding check_file

--- check_mfile.c
+++ check_mfile.c
@@ -4,14 +4,12 @@
  * check_mfile - checks the number of commands and the validity of the monty file
  * @argc: number of arguments
  * @argv: arguments array
- * 
+ *
  * Return: nothing 
 */
 
 void check_mfile(int ac)
 {
-	// int file_desc;
-
 	if (ac != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
--- main.c
+++ main.c
@@ -9,8 +9,26 @@
  
 int main(int argc, char *argv[])
 {
+	FILE *fd;
+	/**char *command[256];*/
+	int line_number = 0;
+
 	check_mfile(argc);
+	fd = fopen(argv[1], "r");
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	size_t buffer_size = 80;
+	char *buffer = malloc(buffer_size * sizeof(char));
+    	while(-1 != getline(&buffer, &buffer_size, fd))
+    	{
+        	printf("%d: %s", ++line_number, buffer);
+    	}
+    	fflush(stdout);
+
 	printf("%s\n", argv[1]);
-	/*read_mfile(argv[1]);*/
+	fclose(fd);
 	return (0);
 }
