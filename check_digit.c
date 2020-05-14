db4f7df102976253ca9314ca21ad1d9ddb2449d8
s-macias <1552@holbertonschool.com>
2020-05-14 05:20:23 +0000

    adding check_digit function

---
+++ check_digit.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+
+/**
+ * check_digit - checks parameter is a number
+ * char *parameter
+ * 
+ * Return: 1 for success, 0 for failure
+ */
+
+int check_digit(char *parameter)
+{
+	while (parameter)
+	{
+		if (isdigit(parameter[i] == 0)
+			return (0);
+		else
+			i++;
+	}
+	return (1);
+}
